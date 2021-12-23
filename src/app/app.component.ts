0ae37579548197b637a9d8cec67413cb645c8269
Miguel Belmonte Granados <mbelmonte@vectoritcgroup.com>
2021-12-23 17:08:42 +0100

    all the ngx-translate over universal configuration + issues resolution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Optional } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Request } from 'express';
+import { REQUEST } from '@nguniversal/express-engine/tokens';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'universalpruebas';
+
+  constructor(
+    private translateService: TranslateService,
+    @Optional() @Inject(REQUEST) private request: Request
+  ) {
+    // settear el uso del lenguage almacenado |
+    //  por defecto setteado por el programador | defecto por el navegador | ingles
+    this.changeCurrentLang(
+      this.request?.cookies?.lang ||
+      // leer document.cookie y buscar lang para obtener el almacenado
+        this.translateService.getDefaultLang() ||
+        this.translateService.getBrowserLang() ||
+        'en'
+    );
+  }
+
+  changeCurrentLang(lang: string) {
+    this.translateService.use(lang);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, BrowserTransferStateModule, TransferState } from '@angular/platform-browser';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+import { TransferHttpCacheModule } from '@nguniversal/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { translateBrowserLoaderFactory } from './translate-browser.loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    TransferHttpCacheModule,
+    BrowserTransferStateModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: translateBrowserLoaderFactory,
+        deps: [HttpClient, TransferState],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
